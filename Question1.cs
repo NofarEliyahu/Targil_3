ff0a3351402a0748577152e84e17fe6772b1fb98
NofarEliyahu <nofareli12@gmail.com>
2026-04-26 14:23:27 +0300

    Add files via upload

---
+++ Question1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Targil_3_1
+{
+    class Account
+    {
+        protected int accountNumber;
+        protected double balance;
+
+        public Account(int accountNumber, double balance)
+        {
+            this.accountNumber = accountNumber;
+            this.balance = balance;
+        }
+
+        public virtual void Withdraw(double amount)
+        {
+            if (balance - amount >= 0)
+            {
+                balance -= amount;
+                Console.WriteLine("Withdraw completed");
+            }
+            else
+            {
+                Console.WriteLine("Not enough money");
+            }
+
+            Console.WriteLine("Balance: " + balance);
+        }
+    }
+
+    class VipAccount : Account
+    {
+        public VipAccount(int accountNumber, double balance)
+            :base(accountNumber, balance)
+        {
+
+        }
+
+        public override void Withdraw(double amount)
+        {
+            if (balance - amount >= -5000)
+            {
+                balance -= amount;
+                Console.WriteLine("VIP withdraw completed");
+            }
+            else
+            {
+                Console.WriteLine("Not enough money");
+            }
+
+            Console.WriteLine("Balance: " + balance);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account regularAccount = new Account(111, 1000);
+            VipAccount vipAccount = new VipAccount(222, 1000);
+
+            Console.WriteLine("Regular Account:");
+            regularAccount.Withdraw(2000);
+
+            Console.WriteLine();
+
+            Console.WriteLine("VIP Account:");
+            vipAccount.Withdraw(2000);
+        }
+    }
+}
---
+++ Question2.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace ConsoleApp62
+{
+    class Aircraft
+    {
+        protected string flightNumber;
+        protected int altitude;
+
+        public Aircraft(string flightNumber, int altitude)
+        {
+            this.flightNumber = flightNumber;
+            this.altitude = altitude;
+        }
+
+        public virtual void PrintStatus()
+        {
+            Console.WriteLine("Flight number: " + flightNumber);
+            Console.WriteLine("Altitude: " + altitude);
+        }
+    }
+
+    class PassengerPlane : Aircraft
+    {
+        private int passengersCount;
+
+        public PassengerPlane(string flightNumber, int altitude, int passengersCount)
+            : base(flightNumber, altitude)
+        {
+            this.passengersCount = passengersCount;
+        }
+
+        public override void PrintStatus()
+        {
+            Console.WriteLine("Passenger Plane");
+            Console.WriteLine("Flight number: " + flightNumber);
+            Console.WriteLine("Altitude: " + altitude);
+            Console.WriteLine("Passengers count: " + passengersCount);
+        }
+    }
+
+    class FighterJet : Aircraft
+    {
+        private bool missilesLoaded;
+
+        public FighterJet(string flightNumber, int altitude, bool missilesLoaded)
+            : base(flightNumber, altitude)
+        {
+            this.missilesLoaded = missilesLoaded;
+        }
+
+        public override void PrintStatus()
+        {
+            Console.WriteLine("Fighter Jet");
+            Console.WriteLine("Flight number: " + flightNumber);
+            Console.WriteLine("Altitude: " + altitude);
+            Console.WriteLine("Missiles loaded: " + missilesLoaded);
+        }
+
+        public void FireMissile()
+        {
+            Console.WriteLine("Fired Missile!");
+            missilesLoaded = false;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aircraft[] arr = new Aircraft[4];
+
+            arr[0] = new PassengerPlane("LY101", 30000, 180);
+            arr[1] = new PassengerPlane("LY202", 28000, 150);
+            arr[2] = new FighterJet("F16", 40000, true);
+            arr[3] = new FighterJet("F35", 45000, true);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i].PrintStatus();
+
+                if (arr[i] is FighterJet)
+                {
+                    FighterJet f = (FighterJet)arr[i];
+                    f.FireMissile();
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
